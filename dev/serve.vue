b0e098cb60453095c8ff408069ca9fb333765ad2
Yusef Richard <yusef.rick@gmail.com>
2021-09-12 21:18:19 -0300

    initial

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { MgSharedComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  MgSharedComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <mg-shared-components-sample />
+  </div>
+</template>
