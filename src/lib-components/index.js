0dc337b776ed189b177008dd98afc3d0e7a0ac0b
Yusef Richard <yusef.rick@gmail.com>
2021-09-13 00:45:43 -0300

    exporting policy

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as MgSharedComponentsSample } from './mg-shared-components-sample.vue';
+export { default as MgSharedPolicy } from './mg-shared-policy.vue';
