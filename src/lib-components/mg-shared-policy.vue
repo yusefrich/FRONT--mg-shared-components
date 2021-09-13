7325ed6fd7b8108619112df00f9926e0605092c4
Yusef Richard <yusef.rick@gmail.com>
2021-09-12 23:57:33 -0300

    added policy component

---
+++ src/lib-components/mg-shared-policy.vue
@@ -0,0 +1,13 @@
+<script>
+
+export default /*#__PURE__*/{
+  name: 'MgSharedPolicy', // vue component name
+  props: {
+    content: String
+  }
+}
+</script>
+
+<template>
+  <div class="privacy-container" v-html="content" />
+</template>
